6fb34eac9247b0614c36babc6dd37f575b2fe919
Kevin Rae <kevin.rae@powerreviews.com>
2016-08-08 13:24:38 -0700

    Writing selected characters to panel UI.  Needs cleanup.  But alpha demo working.

---
+++ bootstrap-3.3.6/docs/app/identificationkey/key.js
@@ -0,0 +1,13 @@
+function init() {
+  $('a.character').click( addCharacter );
+}
+
+//body > div.character-panel > div > div.panel-body > div > ul > li
+///html/body/div[2]/div/div[2]/div/ul
+//body > div.character-panel > div > div.panel-body > div > ul
+
+function addCharacter() {
+  var CharacterName = $(this).prop('id');
+//  alert(CharacterName);
+  $('#character-panel-ul-id').append('<li>'+CharacterName+'</li>');
+}
