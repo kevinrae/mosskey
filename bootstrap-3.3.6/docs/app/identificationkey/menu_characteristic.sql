4cad1623fa2f1d4db8aa4de4afc9875e256b9db8
Kevin Rae <kevin.rae@powerreviews.com>
2018-01-22 12:59:35 -0800

    rm unused files in root project dir. mostly pkg files for node, bootstrap distributions.

--- bootstrap-3.3.6/docs/app/identificationkey/menu_characteristic.sql
+++
@@ -1,4 +0,0 @@
-CREATE VIEW `menu_characteristic` AS
-SELECT *, CONCAT(CharacterMenuGroup.CharacteristicMenuGroup,' ',
- characterisiticsubmenugroup.characteristicsubmenugroup,' ',characteristic.characteristic) 
- AS menu_characteristic_name FROM characteristicmenugroup, characteristicmenusubgroup, characteristic;
--- bootstrap-3.3.6/docs/app/identificationkey/taxon_name.sql
+++
@@ -1,2 +0,0 @@
-CREATE VIEW taxon_name AS 
-SELECT *, CONCAT(taxa.genus,' ',taxa.species,' ',taxa.subspecies) AS taxon_name FROM taxa;
